b5f10dbcf0c8513edc6cdc08d7868bc82828f8b4
Normano1101 <normanbm123@gmail.com>
2025-01-30 15:02:16 -0600

    MixMatch.java
    here is my code for lab 2

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -1,7 +1,27 @@
 //1.Make Public class
 public class MixMatch{
   public static void main(String[] args){
-    int 
+    String varString = "My Name is Norman Madrid" ;
+    char varChar = 'N';
+    short varShort = 12123;
+    int varInt = 23;
+    long varLong = 23000000L;
+    float varFloat = 23.125015F;
+    double varDouble = 24.1235;
+    boolean varB_True = true;
+    boolean varB_False = false;
+    
+    System.out.println(varString + " <- is a variable that can carry a sequence of characters");
+    System.out.println(varChar + " <- is a variable that can carry a single letter, symbol, or digit");
+    System.out.println(varShort + " <- is a variable that can store whole numbers ranging from -32,768 to 32,767");
+    System.out.println(varInt + " <- is a variable that can store whole numbers from -2,147,483,648 to 2,147,483,647");
+    System.out.println(varLong + " <- is a variable that can store whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+    System.out.println(varFloat + " <- is a variable that can store decimal numbers with 6-7 digits in precision");
+    System.out.println(varDouble + " <- is a variable that can  store decimal numbers with 15-16 digits in precision");
+    System.out.println(varB_True + " <- is a variable that represents True ");
+    System.out.println(varB_False + " <- is a variable that represents False");
+    
+  
   }
 }  
     
